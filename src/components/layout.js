d7f062618a4ea879e9f8167c487f4517e22f866b
Emiliano Errecalde <eerrecalde@gmail.com>
2020-12-24 19:21:57 +0000

    Adding cloudinary into the mix + new page posters-viewer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,19 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 import Header from "./header";
 import PosterGrid from "./poster-grid";
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div className="">
+      <Helmet>
+        <script
+          src="https://widget.cloudinary.com/v2.0/global/all.js"
+          type="text/javascript"
+        ></script>
+      </Helmet>
       <Header />
-      <PosterGrid />
+      {children}
     </div>
   );
 };
